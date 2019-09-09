c87e94b2f082e0c64cab5b7a931186eee5383c20
RMAISCO <2280385683@qq.com>
2019-09-09 16:34:13 +0800

    fix login redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Validate the user login request.
