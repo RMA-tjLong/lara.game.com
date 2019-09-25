a7e61c6f35a5daf6cca4288be4d14660d99aaa7b
RMA-tjLong <2839423901@qq.com>
2019-09-25 23:08:19 +0800

    完成了左侧工具栏样式

--- resources/views/public/left-tools.blade.php
+++ resources/views/public/left-tools.blade.php
@@ -1,5 +1,6 @@
 <div id="left-tools">
-    <i class="fa fa-edit"></i>
-    <i class="fa fa-edit"></i>
-    <i class="fa fa-edit"></i>
+    <li title="我的游戏"><i class="fa fa-gamepad"></i></li>
+    <li title="我的最爱"><i class="fa fa-heart"></i></li>
+    <li title="编辑背景"><i class="fa fa-edit"></i></li>
+    <li title="回到顶部"><i class="fa fa-chevron-up"></i></li>
 </div>
