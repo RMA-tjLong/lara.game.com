3581e2c55f68ea3d342f75f9fcd1125534835977
RMA-tjLong <2839423901@qq.com>
2019-09-29 23:29:57 +0800

    增加了公共文章头部样式

--- resources/views/modules/news/index.blade.php
+++ resources/views/modules/news/index.blade.php
@@ -1,13 +1,25 @@
 @extends('layouts.module')
 @section($entityCode . '.active', 'active')
 @section('style')
+    @parent
+    <style>
+    </style>
 @endsection
 
 @section('content')
 	@parent
     <div id="context">
         <div class="container">
-            helloworld
+            <div class="row">
+                <div class="context-header">
+                    <div class="bread-crumbs">
+                        <a href="#">所有产品</a> &gt;
+                        <a href="#">新闻</a>
+                    </div>
+                    <h1 class="page-title">所有新闻</h1>
+                </div>
+            </div>
+
         </div>
     </div>
 @endsection
