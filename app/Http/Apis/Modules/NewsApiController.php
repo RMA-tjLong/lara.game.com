bfc268a2963e770800dc1f4a4d348046c4670299
RMAISCO <2280385683@qq.com>
2019-11-14 14:28:04 +0800

    改了下变量名

--- app/Http/Apis/Modules/NewsApiController.php
+++ app/Http/Apis/Modules/NewsApiController.php
@@ -20,24 +20,24 @@ class NewsApiController extends Controller
         $input = $request->input();
         $input = FiltersUtil::parseFilters($input);
 
-        $data = NewsModel::with(['relate_games.relate_game_titles', 'relate_news_contents', 'relate_news_titles']);
+        $news = NewsModel::with(['relate_games.relate_game_titles', 'relate_news_contents', 'relate_news_titles']);
 
         if ($input['time']) {
-            $data = $data->whereBetween('created_at', $input['time']);
+            $news = $news->whereBetween('created_at', $input['time']);
         }
 
         if ($news_tag_code = $input['tag']) {
             // 当标签存在时检索对应新闻，当标签不存在时返回所有新闻
             if (NewsTagsModel::where(['code' => $news_tag_code])->first()) {
-                $data = $data->whereHas('relate_news_tags', function($query) use ($news_tag_code) {
+                $news = $news->whereHas('relate_news_tags', function($query) use ($news_tag_code) {
                     $query->where('code', $news_tag_code);
                 });
             }
         }
 
-        $data = $data->paginate();
+        $news = $news->paginate();
 
-        return ajax_return_success($data);
+        return ajax_return_success($news);
     }
 }
 
